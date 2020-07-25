c831ec241a4dd39760736f9a4e20ba1fbdbb45f9
Charithra <you@example.com>
2020-07-25 20:31:32 +0530

    headerStyle

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import GoogleAuth from './GoogleAuth';
+import { Link} from 'react-router-dom';
+import GoogleAuth from '../GoogleAuth';
+
+import './Header.scss';
 
 const Header = () => {
     return (
         <div className="ui secondary pointing menu">
-            <Link to="/" className="item">Streamy</Link>
+            <Link to="/" className="shownItem">Streamy</Link>
             <div className="right menu">
-                <Link to="/" className="item">All Streams</Link>
+                <Link to="/" className="shownItem">All Streams</Link>
                 <Link to="/" className="item">
                     <GoogleAuth/>
                 </Link>
